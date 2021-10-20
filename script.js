6484c6facd0276eea86c0dc4526820f4d4739ce3
Jantina Eefting <65661797+JantinaG@users.noreply.github.com>
2021-10-20 12:58:12 +0100

    forEach loops

---
+++ script.js
@@ -0,0 +1,15 @@
+var books = [
+  { title: "Harry Potter", author: "JK Rowling", pages: 300 },
+  { title: "Green eggs and ham", author: "Dr. Seuss", pages: 25 },
+  {
+    title: "The 4 hour work week",
+    author: "Tim Ferris",
+    pages: 400,
+  },
+];
+
+books.forEach(function (book) {
+  document.write(
+    `${book.title} is written by ${book.author} and has ${book.pages} pages.`
+  );
+});
